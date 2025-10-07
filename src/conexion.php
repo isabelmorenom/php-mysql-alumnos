3389a0ea307b1bd9f173035fcd5ddf01dd0badd8
María Isabel Moreno Muñoz <maria.moreno39@educa.madrid.org>
2025-10-07 22:41:12 +0200

    Update conexion.php con conexión PDO

--- src/conexion.php
+++ src/conexion.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "db";
-$username = "root";
-$password = "example";
-$dbname = "miapp";
+$dsn = 'mysql:host=db;dbname=miapp;charset=utf8mb4';
+$username = 'root';
+$password = 'example';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('❌ Error de conexión: ' . $e->getMessage());
 }
 ?>
